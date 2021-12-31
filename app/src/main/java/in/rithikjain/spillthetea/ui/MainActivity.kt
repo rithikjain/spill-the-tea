706cee9bd770ebf899fe8b9738170d08d1640325
Rithik Jain <rithik.jain3006@gmail.com>
2021-12-31 23:14:20 +0530

    feat: setup bottom navigation with navcontroller

--- app/src/main/java/in/rithikjain/spillthetea/ui/MainActivity.kt
+++ app/src/main/java/in/rithikjain/spillthetea/ui/MainActivity.kt
@@ -1,12 +1,23 @@
 package `in`.rithikjain.spillthetea.ui
 
 import `in`.rithikjain.spillthetea.R
-import androidx.appcompat.app.AppCompatActivity
+import `in`.rithikjain.spillthetea.databinding.ActivityMainBinding
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/in/rithikjain/spillthetea/ui/feed/FeedFragment.kt
+++ app/src/main/java/in/rithikjain/spillthetea/ui/feed/FeedFragment.kt
@@ -6,14 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import `in`.rithikjain.spillthetea.R
+import `in`.rithikjain.spillthetea.databinding.FragmentFeedBinding
 
 class FeedFragment : Fragment() {
+
+    private var _binding: FragmentFeedBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+    ): View {
+        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
