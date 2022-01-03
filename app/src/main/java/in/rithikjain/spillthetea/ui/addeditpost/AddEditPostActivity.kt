899be31b0a2dc21dab6ce85187cf81eaf0dc5de8
Rithik Jain <rithik.jain3006@gmail.com>
2022-01-03 17:06:13 +0530

    feat: add channel to handle events on the add edit post activity

--- app/src/main/java/in/rithikjain/spillthetea/ui/addeditpost/AddEditPostActivity.kt
+++ app/src/main/java/in/rithikjain/spillthetea/ui/addeditpost/AddEditPostActivity.kt
@@ -4,7 +4,11 @@ import `in`.rithikjain.spillthetea.databinding.ActivityAddEditPostBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class AddEditPostActivity : AppCompatActivity() {
@@ -21,11 +25,23 @@ class AddEditPostActivity : AppCompatActivity() {
         binding.spillButton.setOnClickListener {
             val content = binding.contentTextField.editText!!.text.toString()
             viewModel.insertPost(content)
-            finish()
         }
 
         binding.closeButton.setOnClickListener {
             finish()
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.addEditPostEvent.collect { event ->
+                when (event) {
+                    is AddEditPostViewModel.AddEditPostEvent.ShowErrorMessage -> {
+                        Snackbar.make(binding.root, event.message, Snackbar.LENGTH_SHORT).show()
+                    }
+                    is AddEditPostViewModel.AddEditPostEvent.PostInsertionSuccess -> {
+                        finish()
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/in/rithikjain/spillthetea/ui/addeditpost/AddEditPostViewModel.kt
+++ app/src/main/java/in/rithikjain/spillthetea/ui/addeditpost/AddEditPostViewModel.kt
@@ -5,6 +5,8 @@ import `in`.rithikjain.spillthetea.data.repository.PostRepository
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
@@ -14,13 +16,26 @@ class AddEditPostViewModel @Inject constructor(
     private val repository: PostRepository
 ) : ViewModel() {
 
+    private val addEditPostEventChannel = Channel<AddEditPostEvent>()
+    val addEditPostEvent = addEditPostEventChannel.receiveAsFlow()
+
     fun insertPost(content: String) {
         if (content.trim().isNotEmpty()) {
             viewModelScope.launch {
                 repository.insertPost(
                     Post(content.trim(), Date())
                 )
+                addEditPostEventChannel.send(AddEditPostEvent.PostInsertionSuccess)
+            }
+        } else {
+            viewModelScope.launch {
+                addEditPostEventChannel.send(AddEditPostEvent.ShowErrorMessage("Content can't be empty '_'"))
             }
         }
+    }
+
+    sealed class AddEditPostEvent {
+        data class ShowErrorMessage(val message: String) : AddEditPostEvent()
+        object PostInsertionSuccess : AddEditPostEvent()
     }
 }
